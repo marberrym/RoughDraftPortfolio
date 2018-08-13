f5ad265d6d4ec00fc666ee8172876635be7f2e39
Matt <marberrym@gmail.com>
2018-08-13 15:39:01 -0400

    Added the side menu bar and transitions.  Still debating how I want my overall portfolio to look stylistically.

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 var menuButton = document.querySelector('#menuIcon');
 var menuBox = document.querySelector('#menuBox');
+var mainPic = document.querySelector('.mainPic');
+var mainFlex = document.querySelector('.mainPageFlex');
+var navBar = document.querySelector('.navBar');
 
+var menuUp = "off";
 var showMenu = function(event) {
-    menuBox.classList.add('menuVisible');
+    if(menuUp === "off") {
+        menuBox.classList.add('menuVisible');
+        menuUp = "on";
+    }
 }
 
-menuButton.addEventListener('click', showMenu)
+var hideMenu = function(event) {
+    if(menuUp === "on") {
+        if(event.target === mainFlex) {
+            menuBox.classList.remove('menuVisible');
+            menuUp = "off";
+        }
+    }
+    
+}
+
+menuButton.addEventListener('click', showMenu);
+window.addEventListener('click', hideMenu);
+
+
